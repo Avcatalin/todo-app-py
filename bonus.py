710a5390afb42215a40aff0b2051b1e5fc84734d
Avcatalin <avarvareicatalin@gmail.com>
2025-01-22 19:41:55 +0200

    updated code mac

--- bonus.py
+++ bonus.py
@@ -1,5 +1,32 @@
-languages = ['English', 'German', 'Spanish']
+password = input("Enter new password: ")
 
-for lang in languages:
-    with open(f"{lang}.txt", "w") as file:
-        file.write(lang)
+result = {}
+
+# check length of the password
+if len(password) >= 8:
+    result["length"] = True
+else:
+    result["length"] = False
+
+# check if isdigit
+digit = False
+
+for i in password:
+    if i.isdigit():
+        digit = True
+
+result["digits"] = digit
+
+# check if is uppercase
+uppercase = False
+
+for i in password:
+    if i.isupper():
+        uppercase = True
+
+result["upper-case"] = uppercase
+
+if all(result.values()):
+    print("Strong Password")
+else:
+    print("Weak Password")
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-todos = []
-
 while True:
     # Get user input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -11,13 +9,13 @@ while True:
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + "\n")
 
         # Write todos in the file
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    if 'show' in user_action:
+    elif 'show' in user_action:
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
@@ -26,8 +24,8 @@ while True:
             item = item.strip('\n')
             row = f"{index + 1}-{item}"
             print(row)
-    if 'edit' in user_action:
-        number = int(input("Number of the todo to edit: "))
+    elif 'edit' in user_action:
+        number = int(user_action[5:])
         number = number - 1
 
         with open('todos.txt', 'r') as file:
@@ -39,8 +37,8 @@ while True:
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    if 'complete' in user_action:
-        number = int(input("Number of the todo to complete: "))
+    elif 'complete' in user_action:
+        number = int(user_action[9:])
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
@@ -54,7 +52,9 @@ while True:
 
         message = f"Todo {todo_to_remove} was removed from the list."
         print(message)
-    if 'exit' in user_action:
+    elif 'exit' in user_action:
         break
+    else:
+        print("Command is not valid.")
 
 print("Goodbye")
