3af2bc58087afa956de6a36f184afbe0513e01c2
Avcatalin <avarvareicatalin@gmail.com>
2025-01-23 21:03:51 +0200

    updated 23.01

--- bonus.py
+++ bonus.py
@@ -1,32 +1,4 @@
-password = input("Enter new password: ")
+filenames = ["report.txt", "downloads.txt", "success.txt", "folders.txt"]
 
-result = {}
-
-# check length of the password
-if len(password) >= 8:
-    result["length"] = True
-else:
-    result["length"] = False
-
-# check if isdigit
-digit = False
-
-for i in password:
-    if i.isdigit():
-        digit = True
-
-result["digits"] = digit
-
-# check if is uppercase
-uppercase = False
-
-for i in password:
-    if i.isupper():
-        uppercase = True
-
-result["upper-case"] = uppercase
-
-if all(result.values()):
-    print("Strong Password")
-else:
-    print("Weak Password")
+for file in filenames:
+    print(file[:-4].title())
