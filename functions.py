a7cd85be1f049ed52857c1d22303efab9505ee1a
Avcatalin <avarvareicatalin@gmail.com>
2025-01-26 19:32:49 +0200

    update 26.01

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Read  a text file and return the list of to-do items. """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
@@ -6,7 +8,7 @@ def get_todos(filepath="todos.txt"):
 
 
 # Function to open file and write new lines
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
